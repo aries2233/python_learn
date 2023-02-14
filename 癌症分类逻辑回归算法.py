bca774eddb5c9837543655067c93e34ee5fbe612
aries2233 <122298280+aries2233@users.noreply.github.com>
2023-02-14 23:28:32 +0800

    逻辑回归与二分类学习

---
+++ 癌症分类逻辑回归算法.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+#获取数据
+path="https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data"
+column_name=['']
+pd.read_csv(path,names)
+#数据处理（缺失值和names）
+#数据集划分
+#特征工程————无量纲化：标准化
+#逻辑回归预估器
+#模型评估
